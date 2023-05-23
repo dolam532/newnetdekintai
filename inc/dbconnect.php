58e4124a74fa1034db2eac1c1b302eb0084275ba
Marucho2020 <115078558+Marucho2020@users.noreply.github.com>
2023-05-23 14:22:28 +0900

    demo test commit bao

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -2,6 +2,8 @@
 // connect to database
 $conn = mysqli_connect('153.127.255.167', 'ndk', 'ganandkadm2019', 'newndk');
 
+//Demo Test Commit BAO 
+
 // check connection
 if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
