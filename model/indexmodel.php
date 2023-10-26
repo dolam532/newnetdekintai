7e0e770ff7faae74666031644d9adf92dee40b97
Daniel MungSen <danielmungsen2019@gmail.com>
2023-10-26 17:12:35 +0900

    Update of index New message show

--- model/indexmodel.php
+++ model/indexmodel.php
@@ -1,10 +1,16 @@
 <?php
 // Select data from tbl_notice
 // (index.php)
-$sql_notice = 'SELECT * FROM `tbl_notice`
+$sql_notice = 'SELECT
+`tbl_notice`.*,
+`tbl_user`.`reg_dt` AS user_reg
+FROM
+`tbl_notice`
 JOIN `tbl_user` ON `tbl_notice`.`uid` = `tbl_user`.`uid`
-WHERE `tbl_notice`.`uid` = `tbl_user`.`uid`
-ORDER BY `tbl_notice`.`bid`';
+WHERE
+`tbl_notice`.`uid` = `tbl_user`.`uid`
+ORDER BY
+`tbl_notice`.`bid`';
 
 $result_notice = mysqli_query($conn, $sql_notice);
 $notice_list = mysqli_fetch_all($result_notice, MYSQLI_ASSOC);
