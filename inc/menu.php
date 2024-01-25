1ea381ad78d6d115c60b2de4ba7b642e5078c6bb
Daniel MungSen <danielmungsen2019@gmail.com>
2024-01-25 15:29:38 +0900

    no message

--- inc/menu.php
+++ inc/menu.php
@@ -77,7 +77,6 @@
                             <ul class="dropdown-menu">
                                 <li><a href="../manage/manageInfo.php">会社情報登録</a></li>
                                 <li><a href="../manage/companyList.php">使用者登録</a></li>
-                                <li><a href="../manage/companywList.php">業務時間登録</a></li>
                             </ul>
                         </li>
                     <?php elseif ($_SESSION['auth_type'] == constant('ADMIN') || $_SESSION['auth_type'] == constant('ADMINISTRATOR')) : ?>
@@ -86,7 +85,6 @@
                             <ul class="dropdown-menu">
                                 <li><a href="../manage/manageInfo.php">会社情報編集</a></li>
                                 <li><a href="../manage/companyList.php">使用者編集</a></li>
-                                <li><a href="../manage/companywList.php">業務時間編集</a></li>
                                 <li><a href="../manage/adminList.php">管理者登録</a></li>
                                 <li><a href="../manage/kyukaNotice.php">休暇お知らせ登録</a></li>
                                 <li><a href="../manage/kyukaInfo.php">年次有給休暇日数登録</a></li>
