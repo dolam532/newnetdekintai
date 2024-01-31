0ed97f30cf624270ab0b4c3b2a88616f83ac0679
Daniel MungSen <danielmungsen2019@gmail.com>
2024-01-31 12:28:12 +0900

    no message

--- pdfdownload/generatekyukapdf.php
+++ pdfdownload/generatekyukapdf.php
@@ -434,6 +434,7 @@ $yTmp12 = $tcpdf->GetY();
 $tcpdf->Cell(30, 8, "\n" . $infotitlebottom, 1, 'C', true);
 $tcpdf->SetXY($xTmp12 + 30, $yTmp12);
 $height12 = $tcpdf->GetY() - $yTmp12;
+
 foreach ($kyukaInfoListbottomArray as $key => $value) {
 	if (count($kyukaInfoListbottomArray) < 8) {
 		$tcpdf->Cell(20, 8, $value, 1, 'C', true);
@@ -443,6 +444,7 @@ foreach ($kyukaInfoListbottomArray as $key => $value) {
 		}
 		if ($key >= 8) {
 			$tcpdf->Cell(0, 8, '...', 0, 1, 'L');
+			break;
 		}
 	}
 }
